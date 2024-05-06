38a572d11c51f3973664997d0695175bde85aa90
ChisAndreiCalin <100203543+ChisAndreiCalin@users.noreply.github.com>
2024-05-06 15:00:43 +0300

    Add files via upload

--- Chatbot_Licenta/Licenta-Chatbot/Chatbot_Licenta/run_chatbot.py
+++ Chatbot_Licenta/Licenta-Chatbot/Chatbot_Licenta/run_chatbot.py
@@ -2,11 +2,10 @@ from ChatbotClass.Chatbot2 import Chatbot
 from constants import *
 
 if __name__ == "__main__":
-    csvmedicamente = DATASET_FOLDER + SEPARATOR + "Medicamente_Csv.csv"
+    csvmedicamente = DATASET_FOLDER + SEPARATOR + "MedicamenteCsv2.csv"
     csvboli = DATASET_FOLDER + SEPARATOR + "CSVBoli.csv"
     csvuser = DATASET_FOLDER + SEPARATOR + "userCsv.csv"
     csvraspunsuri = DATASET_FOLDER + SEPARATOR + "responce.csv"
     csvmistakes = DATASET_FOLDER + SEPARATOR + "mistakes.csv"
-    csvschedule = DATASET_FOLDER + SEPARATOR + "Schedule.csv"
-    chatbot = Chatbot(csvuser, csvboli, csvmedicamente, csvraspunsuri, csvuser, csvschedule)
+    chatbot = Chatbot(csvuser, csvboli, csvmedicamente, csvraspunsuri, csvuser)
     chatbot.start()
